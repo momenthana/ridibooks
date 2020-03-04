f8e6ffa0614dd8891480f5124439debda5984986
Hana <momenthana@kakao.com>
2020-03-05 02:26:27 +0900

    koa init

--- svr/index.js
+++ svr/index.js
@@ -1,1 +1,13 @@
-console.log('init')
+const Koa = require('koa')
+const app = new Koa()
+
+app.use(async (ctx, next) => {
+  ctx.body = 'Hello Koa'
+  const start = new Date()
+  await next()
+  console.log(new Date() - start + 'ms')
+})
+
+app.listen(3000, () => {
+  console.log('heurm server is listening to port 3000')
+})
