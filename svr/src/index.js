3631d19b8737de3cec3ea789759a0121bd701ef8
Hana <momenthana@kakao.com>
2020-03-05 04:51:52 +0900

    koa-router 적용

--- svr/src/index.js
+++ svr/src/index.js
@@ -1,13 +1,21 @@
 const req = require('./request')
 const Koa = require('koa')
+const Router = require('koa-router')
+
 const app = new Koa()
+const router = new Router()
+
+router.get('/', async (ctx, next) => {
+  ctx.body = await req()
+})
 
 app.use(async (ctx, next) => {
-  ctx.body = await req()
   const start = new Date()
   await next()
   console.log(new Date() - start + 'ms')
 })
+
+app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(process.env.PORT, () => {
   console.log(`heurm server is listening to port ${process.env.PORT}`)
