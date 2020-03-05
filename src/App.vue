4cb606699c948522dfaeb860de50ab92cbaf84f6
Hana <momenthana@kakao.com>
2020-03-05 19:03:48 +0900

    App bar 생성

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <template>
   <v-app>
+    <v-app-bar
+      absolute
+      color="#1F8BE4"
+      dark
+      shrink-on-scroll
+      prominent
+      fade-img-on-scroll
+      scroll-target="#scrolling-techniques"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>RIDIBOOKS</v-toolbar-title>
+
+      <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab>EVENT</v-tab>
+        </v-tabs>
+      </template>
+    </v-app-bar>
     <Main/>
   </v-app>
 </template>
