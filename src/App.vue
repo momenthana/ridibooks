0bf15abefe1d803b4ee73de088c3df8523ce0f87
Hana <momenthana@kakao.com>
2020-03-05 19:12:11 +0900

    Bar 컴포넌트 분리

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,19 @@
 <template>
   <v-app>
-    <v-app-bar
-      absolute
-      color="#1F8BE4"
-      dark
-      shrink-on-scroll
-      prominent
-      fade-img-on-scroll
-      scroll-target="#scrolling-techniques"
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-      <v-toolbar-title>RIDIBOOKS</v-toolbar-title>
-
-      <template v-slot:extension>
-        <v-tabs align-with-title>
-          <v-tab>EVENT</v-tab>
-        </v-tabs>
-      </template>
-    </v-app-bar>
+    <Bar/>
     <Main/>
   </v-app>
 </template>
 
 <script>
+import Bar from './components/Bar'
 import Main from './components/Main'
 
 export default {
   name: 'App',
 
   components: {
+    Bar,
     Main
   },
 
---
+++ src/components/Bar.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app-bar
+    app
+    color="#1F8CE6"
+    dark
+    shrink-on-scroll
+    prominent
+    fade-img-on-scroll
+    scroll-target="#scrolling-techniques"
+  >
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+    <v-toolbar-title>RIDIBOOKS</v-toolbar-title>
+
+    <template v-slot:extension>
+      <v-tabs
+        align-with-title
+        color="white"
+      >
+        <v-tab>EVENT</v-tab>
+      </v-tabs>
+    </template>
+  </v-app-bar>
+</template>
