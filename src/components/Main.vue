1d210ca09148a67f61cf2be9ea08532bd86e0644
Hana <momenthana@kakao.com>
2020-03-06 01:35:39 +0900

    카드 디자인 추가

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,11 +1,82 @@
 <template>
   <v-content>
-    1
+    <v-sheet
+      id="scrolling-techniques"
+      class="overflow-y-auto"
+      max-height="100vh"
+    >
+    <v-container>
+      <v-card
+        class="mx-auto"
+        max-width="400"
+        elevation="9"
+      >
+        <v-list-item two-line>
+          <v-list-item-content>
+            <v-list-item-title class="headline">선릉역</v-list-item-title>
+            <v-list-item-subtitle>Mon, 12:30 PM, Mostly sunny</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-card-text>
+          <v-row align="center">
+            <v-col class="display-3" cols="6">
+              23&deg;C
+            </v-col>
+            <v-col cols="6">
+              <v-icon x-large color="orange">mdi-white-balance-sunny</v-icon>
+            </v-col>
+          </v-row>
+        </v-card-text>
+
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon color="blue">mdi-weather-windy</v-icon>
+          </v-list-item-icon>
+          <v-list-item-subtitle>23 km/h</v-list-item-subtitle>
+        </v-list-item>
+
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon color="blue">mdi-water</v-icon>
+          </v-list-item-icon>
+          <v-list-item-subtitle>48%</v-list-item-subtitle>
+        </v-list-item>
+
+        <v-list class="transparent">
+          <v-list-item
+            v-for="item in forecast"
+            :key="item.day"
+          >
+            <v-list-item-title>{{ item.day }}</v-list-item-title>
+
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-subtitle class="text-right">
+              {{ item.temp }}
+            </v-list-item-subtitle>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-container>
+    </v-sheet>
   </v-content>
 </template>
 
 <script>
 export default {
-  name: 'Main'
+  name: 'Main',
+
+  data () {
+    return {
+      forecast: [
+        { day: 'Tuesday', icon: 'mdi-white-balance-sunny', temp: '24\xB0/12\xB0' },
+        { day: 'Wednesday', icon: 'mdi-white-balance-sunny', temp: '22\xB0/14\xB0' },
+        { day: 'Thursday', icon: 'mdi-cloud', temp: '25\xB0/15\xB0' }
+      ]
+    }
+  }
 }
 </script>
