8c6358f2fca50a77f11151ecdf029b1d90a19cd5
Hana <momenthana@kakao.com>
2020-06-12 19:37:03 +0900

    필요한 날씨 정보만 추출

--- svr/src/request.js
+++ svr/src/request.js
@@ -16,17 +16,22 @@ const save = (info) => {
 const request = {
   live: async () => {
     const data = load()
-    let info
+    const info = {}
     const date = new Date()
     date.setMinutes(date.getMinutes() - 40)
     const baseDate = `${date.getFullYear()}${('0' + (date.getMonth() + 1)).slice(-2)}${('0' + date.getDate()).slice(-2)}`
     const baseTime = `${('0' + date.getHours()).slice(-2)}00`
-    if (data.live && data.live[0].baseTime === baseTime && data.live[0].baseDate === baseDate) {
+    if (data.live && data.live.baseTime === baseTime && data.live.baseDate === baseDate) {
       return data.live
     }
     await axios.get(`http://apis.data.go.kr/1360000/VilageFcstInfoService/getUltraSrtNcst?serviceKey=${process.env.SERVICE_KEY}&dataType=JSON&base_date=${baseDate}&base_time=${baseTime}&nx=61&ny=125`)
       .then(function (res) {
-        info = res.data.response.body.items.item
+        info.baseDate = res.data.response.body.items.item[0].baseDate
+        info.baseTime = res.data.response.body.items.item[0].baseTime
+        const items = res.data.response.body.items.item
+        for (const key in items) {
+          info[items[key].category] = items[key].obsrValue
+        }
       })
     data.live = info
     save(data)
