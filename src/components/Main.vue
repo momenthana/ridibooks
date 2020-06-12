620f0c8fb4901c1ad71a33bb63a8f983a7bfaab5
Hana <momenthana@kakao.com>
2020-06-12 20:12:17 +0900

    Vue data input

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -9,17 +9,17 @@
         <v-list-item two-line>
           <v-list-item-content>
             <v-list-item-title class="headline">선릉역</v-list-item-title>
-            <v-list-item-subtitle>Mon, 12:30 PM, Mostly sunny</v-list-item-subtitle>
+            <v-list-item-subtitle>{{live.baseDate.slice(0, 4)}}년 {{live.baseDate.slice(4, 6)}}월 {{live.baseDate.slice(6, 8)}}일 {{live.baseTime.slice(0, 2)}}시 기준</v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
 
         <v-card-text>
           <v-row align="center">
-            <v-col class="display-3" cols="6">
-              23&deg;C
+            <v-col cols="4">
+              <v-icon size="100" color="blue">mdi-weather-{{live.PTY === '1' ? 'rainy' : live.PTY === '2' ? 'snowy-rainy' : live.PTY === '3' ? 'snowy' : live.PTY === '4' ? 'pouring' : 'cloudy' }}</v-icon>
             </v-col>
-            <v-col cols="6">
-              <v-icon x-large color="orange">mdi-white-balance-sunny</v-icon>
+            <v-col class="display-3">
+              {{live.T1H}}&deg;C
             </v-col>
           </v-row>
         </v-card-text>
@@ -28,32 +28,15 @@
           <v-list-item-icon>
             <v-icon color="blue">mdi-weather-windy</v-icon>
           </v-list-item-icon>
-          <v-list-item-subtitle>23 km/h</v-list-item-subtitle>
+          <v-list-item-subtitle>{{live.WSD}} m/s</v-list-item-subtitle>
         </v-list-item>
 
         <v-list-item>
           <v-list-item-icon>
             <v-icon color="blue">mdi-water</v-icon>
           </v-list-item-icon>
-          <v-list-item-subtitle>48%</v-list-item-subtitle>
+          <v-list-item-subtitle>{{live.REH}}%</v-list-item-subtitle>
         </v-list-item>
-
-        <v-list class="transparent">
-          <v-list-item
-            v-for="item in forecast"
-            :key="item.day"
-          >
-            <v-list-item-title>{{ item.day }}</v-list-item-title>
-
-            <v-list-item-icon>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-item-icon>
-
-            <v-list-item-subtitle class="text-right">
-              {{ item.temp }}
-            </v-list-item-subtitle>
-          </v-list-item>
-        </v-list>
       </v-card>
     </v-container>
   </v-content>
@@ -67,19 +50,13 @@ export default {
 
   data () {
     return {
-      live: [],
-      forecast: [
-        { day: 'Tuesday', icon: 'mdi-white-balance-sunny', temp: '24\xB0/12\xB0' },
-        { day: 'Wednesday', icon: 'mdi-white-balance-sunny', temp: '22\xB0/14\xB0' },
-        { day: 'Thursday', icon: 'mdi-cloud', temp: '25\xB0/15\xB0' }
-      ]
+      live: null
     }
   },
 
   mounted () {
-    axios.get('http://localhost/live')
+    axios.get(process.env.VUE_APP_API_BASE_URL + '/live')
       .then(res => {
-        console.log(res.data)
         this.live = res.data
       })
   }
